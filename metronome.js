2dd5f59687b5030d7b1cab558d6b05e00ba71f5f
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-06-05 18:38:56 -0400

    Create beatsPerMinute array method

--- metronome.js
+++ metronome.js
@@ -2,3 +2,11 @@ function Metronome(tempo, beatsPerMinute){
   this.tempo = tempo;
   this.beatsPerMinute = beatsPerMinute;
 }
+
+Metronome.prototype.beatsPerMinuteArray = function(){
+  var beatsPerMinuteArray = [];
+  for (var i = 1; i <= this.beatsPerMinute; i++){
+    beatsPerMinuteArray.push(i);
+  };
+  return beatsPerMinuteArray;
+}
