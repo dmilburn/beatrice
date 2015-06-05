6869e9ab5b54cc32ef8adcb01d3bcbe57893289c
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-06-05 19:32:08 -0400

    Create start and updateCounterView metronome methods

--- metronome.js
+++ metronome.js
@@ -1,13 +1,17 @@
 function Metronome(tempo, beatsPerMeasure){
-  this.tempo = tempo;
-  this.beatsPerMeasure = beatsPerMeasure;
+  this.tempo = Number(tempo);
+  this.beatsPerMeasure = Number(beatsPerMeasure);
 }
 
-Metronome.prototype.beatsPerMeasureArray = function(){
-  var beatsPerMeasureArray = [];
-  for (var i = 1; i <= this.beatsPerMeasure; i++){
-    beatsPerMeasureArray.push(i);
-  };
-  return beatsPerMeasureArray;
-} 
+Metronome.prototype.start = function(){
+  window.setInterval(this.updateCounterView, 1000, this.beatsPerMeasure);
+}
+Metronome.prototype.updateCounterView = function(beatsPerMeasure){
+  var counter = document.getElementById("metronome-counter");
+  var pastBeat = Number(counter.innerHTML);
+  if (pastBeat < beatsPerMeasure){
+    counter.innerHTML = pastBeat + 1;
+  } else {
+    counter.innerHTML = 1;
+  }
 }
