5684a7a2746bce64504d3ee598c9403315d3211e
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-06-06 15:41:58 -0400

    Change submit event listener to start or stop

--- js/application.js
+++ js/application.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
 
   $(".metronome-parameters").on("submit", function(event){
     event.preventDefault();
-    var tempo = $("#tempo-field").val();
-    var beatsPerMeasure = $("#beats-field").val();
-    metronome = new Metronome(tempo, beatsPerMeasure);
-    metronome.start();
+    if ($('#metronome-button').val() === "Start"){
+      var tempo = $("#tempo-field").val();
+      var beatsPerMeasure = $("#beats-field").val();
+      metronome = new Metronome(tempo, beatsPerMeasure);
+      metronome.start();
+    } else {
+      metronome.stop();
+    }
+
   });
 })
