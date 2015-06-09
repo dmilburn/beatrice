e0d32f511e9c2e9f1e321550c42c5dbf7bf09b94
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-06-09 11:06:13 -0400

    Checkmarks now change the audible subdivisions

--- js/application.js
+++ js/application.js
@@ -6,7 +6,15 @@ $(document).ready(function(){
       $('#metronome-button').val("Stop");
       var tempo = $("#tempo-field").val();
       var beatsPerMeasure = $("#beats-field").val();
-      metronome = new Metronome(tempo, beatsPerMeasure, [2, 3]);
+      var subdivisions = [];
+      var possibleNoteValues = ["#eighths", "#triplets", "#sixteenths", "#quintuplets"];
+      var possibleSubdivisions = [2, 3, 4, 5];
+      for (var i = 0; i < possibleSubdivisions.length; i++){
+        if ($(possibleNoteValues[i])[0].checked){
+          subdivisions.push(possibleSubdivisions[i]);
+        };
+      };
+      metronome = new Metronome(tempo, beatsPerMeasure, subdivisions);
       $('.input-field').hide();
       metronome.start();
     } else {
