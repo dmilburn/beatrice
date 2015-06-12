99e9a2713a4bc8f26334317a67a6ac63a468f500
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-06-12 15:17:10 -0400

    Create remove2if4 method to fix bug with sixteenths and eights

--- js/helper.js
+++ js/helper.js
@@ -7,5 +7,14 @@ createSubdivisionsArray = function(){
       subdivisions.push(possibleSubdivisions[i]);
     };
   };
+  subdivisions = remove2If4(subdivisions);
   return subdivisions
+}
+
+remove2If4 = function(array){
+  if (array.indexOf(2) != -1 && (array.indexOf(4)) != -1) {
+    var index = array.indexOf(2);
+    array.splice(index, 1);
+  };
+  return array;
 }
